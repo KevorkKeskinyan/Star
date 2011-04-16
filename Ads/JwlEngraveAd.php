abb0fd02bbc5fce311623f358af0f140338e15aa
Kevork Keskinyan <kevork@softlinkdevelopment.com>
2011-04-16 14:59:48 -0400

    1st Edit

--- Ads/JwlEngraveAd.php
+++ Ads/JwlEngraveAd.php
@@ -2,7 +2,7 @@
 <div id="engrave">
   <h1>Free Engraving</h1>
   <img src="../images/Ads/FREE_ENGRAVING.jpg" />
-  <p style="text-align:center">StarWeddingBands.com <br />
+  <p style="text-align:center">Star.com <br />
   offers FREE engraving<br />
   with the purchase of a<br />
   wedding band.</p>
