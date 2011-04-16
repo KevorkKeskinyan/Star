427a0ee678e788664c795af9608b0897a441069f
Kevork Keskinyan <kevork@softlinkdevelopment.com>
2011-04-16 14:49:22 -0400

    1st Commit

---
+++ Ads/CopyRightAd.php
@@ -0,0 +1,19 @@
+<!-- CopyRightAd.html : CopyRight Ad -->
+
+<div id="footerCopyRight" class="grid_24" >
+  <div id="bottommenu">
+  <ul >
+   	<li><a href="index.php">&nbsp;Home |&nbsp;</a></li>
+   	<li><a href="index.php">AboutUs |&nbsp;</a></li>
+   	<li><a href="index.php">ContactUs |&nbsp;</a></li>
+   	<li><a href="index.php">Order Track |&nbsp;</a></li>
+    <li><a href="ReturnPolicy.php">Return Policy |&nbsp;</a></li>
+    <li><a href="index.php">Customer Service |&nbsp;</a></li>
+    <li><a href="cart.php">Shopping Cart</a></li>
+  </ul>
+  </div>
+  <br /><br />
+
+<strong>Copyright &copy;&nbsp;2010-2011 StarWeddingBands.com. All Rights Reserved.</strong>
+</div>
+
